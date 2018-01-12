db547370a87b9b2b020c577421c81cde7bb4bed6
Dauren O. Esmukhanov <d.esmukhanov@kbtu.kz>
2018-01-12 11:22:33 +0600

    fix

--- 01/02.py
+++ 01/02.py
@@ -1,15 +1,25 @@
 alpha = {} #dictionary
-#key -> value
-alpha['a'] = '.-'
-alpha['b'] = '--.'
-alpha['c'] = '-.-.'
+def read_morse_table():
+	file = open('morse.txt', 'r') #open file for read
+	lines = file.readlines() # read all lines
+	alpha = {}
+	for line in lines:
+		l = line.lower().split()
+		if len(l) == 2:
+			key, value = l
+			alpha[key] = value
+	return alpha
 
+alpha = read_morse_table()
+alpha[' '] = '.......'
+alpha['.'] = '......'
+alpha[','] = '.-.-.-'
 def text_to_morse(text):
 	r = []
-	for letter in text:
+	for letter in text.lower():
 		if letter in alpha: #if key exists
 			r.append(alpha[letter]) # get value by key
 	return r
 
-t = text_to_morse('abc')
+t = text_to_morse('Aliya')
 print(t)
