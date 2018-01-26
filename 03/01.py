e467c65c7211451d54be1fd94301a76a96a9de61
Dauren O. Esmukhanov <d.esmukhanov@kbtu.kz>
2018-01-26 10:45:53 +0600

    fix

--- 03/01.py
+++ 03/01.py
@@ -1,8 +1,9 @@
 import csv
-
+from collections import Counter
+crime_stat = Counter()
 with open('crime_almaty.csv', encoding='utf-8') as file:
 	reader = csv.DictReader(file, delimiter=',', quotechar='"')
 	for row in reader:
-		print(row)
-		print(row['reg_code'])
-		break
+		crime_stat[row['stat']] +=1
+
+print(crime_stat.most_common(10))
