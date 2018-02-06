02abd50e7d4428000d633659167699a1ec9a001a
Dauren O. Esmukhanov <d.esmukhanov@kbtu.kz>
2018-02-06 16:52:35 +0600

    json

--- 04/03.py
+++ 04/03.py
@@ -6,9 +6,14 @@ data = r.json()
 school_stat = Counter()
 for course_item in data:
 	school_name = course_item['school name']
-	grade_two = course_item['grade_two']
-	school =  school_name + "||" +course_item['region']
-	school_stat[school] += grade_two
+	course_name = course_item['course']
 
-print(school_stat)
+	if course_item['region'] == 'г.Алматы' and ("математика" in course_name.lower() or "алгебра" in course_name.lower() or "геометрия" in course_name.lower()):
+		c = course_item['grade_five']
+		school =  school_name + "||" +course_item['region']
+		school_stat[school] += c
+
+
+l = school_stat.most_common(3)
+print(l)
 
