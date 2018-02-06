09ea94d4e81b1128273b7c83c42f4c88b3cd0e4c
Dauren O. Esmukhanov <d.esmukhanov@kbtu.kz>
2018-02-06 16:46:31 +0600

    json

--- 04/03.py
+++ 04/03.py
@@ -1,3 +1,14 @@
 import requests
-r = requests.get('http://data.egov.kz/api/v2/progress_courses/data?source={"size":100,"from":0}')
-print(r.text)
+import json
+from collections import Counter
+r = requests.get('http://data.egov.kz/api/v2/progress_courses/data?source={"size":2000,"from":0}')
+data = r.json()
+school_stat = Counter()
+for course_item in data:
+	school_name = course_item['school name']
+	grade_two = course_item['grade_two']
+	school =  school_name + "||" +course_item['region']
+	school_stat[school] += grade_two
+
+print(school_stat)
+
