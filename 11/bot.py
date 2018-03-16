d79f2670da322074b25911f34231a5e76f20e424
Dauren O. Esmukhanov <d.esmukhanov@kbtu.kz>
2018-03-16 11:48:02 +0600

    fix

--- 11/bot.py
+++ 11/bot.py
@@ -7,8 +7,7 @@ updater = Updater(token='271801612:AAF_3DZ-QhhFkmi7dAi_TOjkPw5Ok82eINU')
 dispatcher = updater.dispatcher
 
 def start_reply(bot, update):
-	text = get_balance('9643908503307746820')
-	print(update)
+	text = 'Введите 19 цифр вашей Онай карты'
 	bot.send_message(chat_id=update.message.chat_id, text=text)
 
 from telegram.ext import CommandHandler
@@ -17,6 +16,21 @@ dispatcher.add_handler(start_handler)
 
 def echo_reply(bot, update):
 	text = update.message.text.lower()
+	import re
+	p = re.compile('^(96431085033|96439085033)\d{8}$')
+	if p.match(text):
+		card_no = text
+		balance = get_balance(card_no)
+		if balance is not None:
+			text = 'Баланс карты: %d' % balance
+			bot.send_message(chat_id=update.message.chat_id, text=text)
+		else:
+			text = 'Карта не найдена'
+			bot.send_message(chat_id=update.message.chat_id, text=text)
+	else:
+		text = 'Введите 19 цифр вашей Онай карты'
+		bot.send_message(chat_id=update.message.chat_id, text=text)
+
 
 from telegram.ext import MessageHandler, Filters
 echo_handler = MessageHandler(Filters.text, echo_reply)
